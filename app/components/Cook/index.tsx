dceb6d6ea8e6f0e79519131e5c292262fd9f9c2a
Sam Kurian Shaju <samkuriansc.com>
2024-02-04 00:37:58 +0530

    images changed

--- app/components/Cook/index.tsx
+++ app/components/Cook/index.tsx
@@ -10,7 +10,7 @@ const Cook = () => {
             <div className="mx-auto max-w-7xl lg:pt-20 sm:pb-24 px-6">
 
                 <div className='absolute right-0 bottom-[-18%] hidden lg:block'>
-                    <Image src={'/images/Cook/burger.png'} alt="burger-image" width={463} height={622} />
+                    <Image src={'/images/Banner/bnl1.png'} alt="burger-image" width={463} height={622} />
                 </div>
 
                 <div className='grid grid-cols-1 lg:grid-cols-12 my-16 space-x-5'>
@@ -26,12 +26,12 @@ const Cook = () => {
                         </Fade>
                         <Fade direction={'up'} delay={800} cascade damping={1e-1} triggerOnce={true}>
                             <h3 className="text-3xl lg:text-5xl font-semibold text-black text-start">
-                                Cooking together with the expert.
+                                Be with our cake expert.
                             </h3>
                         </Fade>
                         <Fade direction={'up'} delay={1000} cascade damping={1e-1} triggerOnce={true}>
-                            <p className='text-grey md:text-lg font-normal mb-10 text-start mt-2'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem </p>
-                            <p className='text-grey md:text-lg font-normal mb-10 text-start mt-1'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium....</p>
+                            <p className='text-grey md:text-lg font-normal mb-10 text-start mt-2'>Dive into a world of culinary delight with our extensive collection of cake recipes, each meticulously crafted to elevate your dessert experience to new heights. Whether you're craving the timeless simplicity of a fluffy vanilla sponge or the indulgent richness of a triple-layered chocolate ganache masterpiece, our recipes are designed to cater to every palate and occasion.</p>
+                            <p className='text-grey md:text-lg font-normal mb-10 text-start mt-1'>Prepared with the finest ingredients and infused with passion, our cakes are not just desserts but culinary masterpieces designed to bring joy and delight to every moment shared with loved ones.</p>
                             <div className='flex align-middle justify-center md:justify-start'>
                                 <button className='text-xl font-medium rounded-full text-white py-5 px-6 bg-pink lg:px-10 mr-6'>Learn more</button>
                             </div>
